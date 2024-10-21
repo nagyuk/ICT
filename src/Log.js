504ff311459ef999a8a5d1ab12dd18a88983e4b6
nagyuk <nagai200010@yahoo.co.jp>
2024-10-21 13:55:32 +0900

    購入品入力ページの画面遷移ボタンの位置の修正

--- src/Log.js
+++ src/Log.js
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
-import {Box,Heading,Text,VStack,Input,Button,Table,Thead,Tbody,Tr,Th,Td,Container,useToast,Stack,FormControl,FormLabel,HStack,useBreakpointValue,} from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  Input,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Container,
+  useToast,
+  Stack,
+  FormControl,
+  FormLabel,
+  HStack,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Log = () => {
@@ -41,87 +61,100 @@ const Log = () => {
   const buttonSize = useBreakpointValue({ base: 'xs', md: 'sm' });
 
   return (
-    <Container maxW="container.xl" py={4}>
-      <VStack spacing={6} align="stretch">
-        <Heading as="h1" fontSize="2xl" textAlign="center">購入品入力</Heading>
-        
-        <Box as="form" onSubmit={handleSubmit}>
-          <VStack spacing={4} align="stretch">
-            <FormControl>
-              <FormLabel>商品名</FormLabel>
-              <Input
-                placeholder="商品名"
-                value={productName}
-                onChange={(e) => setProductName(e.target.value)}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel>数量</FormLabel>
-              <Input
-                placeholder="数量"
-                type="number"
-                value={quantity}
-                onChange={(e) => setQuantity(e.target.value)}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel>購入日</FormLabel>
-              <Input
-                placeholder="購入日"
-                type="date"
-                value={purchaseDate}
-                onChange={(e) => setPurchaseDate(e.target.value)}
-              />
-            </FormControl>
-            <Button type="submit" colorScheme="blue" w="full">
-              保存
-            </Button>
-          </VStack>
-        </Box>
-
-        <Box>
-          <Heading as="h2" size="lg" mb={4} fontSize="xl">
-            購入履歴
-          </Heading>
-          <Box overflowX="auto">
-            <Table variant="simple" size="sm">
-              <Thead>
-                <Tr>
-                  <Th>商品名</Th>
-                  <Th>数量</Th>
-                  <Th>購入日</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {purchases.map((purchase) => (
-                  <Tr key={purchase.id}>
-                    <Td>{purchase.productName}</Td>
-                    <Td>{purchase.quantity}</Td>
-                    <Td>{purchase.purchaseDate}</Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
+    <Box pb={20}>  {/* Add bottom padding to account for the navigation bar */}
+      <Container maxW="container.xl" py={4}>
+        <VStack spacing={6} align="stretch">
+          <Heading as="h1" fontSize="2xl" textAlign="center">購入品入力</Heading>
+          
+          <Box as="form" onSubmit={handleSubmit}>
+            <VStack spacing={4} align="stretch">
+              <FormControl>
+                <FormLabel>商品名</FormLabel>
+                <Input
+                  placeholder="商品名"
+                  value={productName}
+                  onChange={(e) => setProductName(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>数量</FormLabel>
+                <Input
+                  placeholder="数量"
+                  type="number"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>購入日</FormLabel>
+                <Input
+                  placeholder="購入日"
+                  type="date"
+                  value={purchaseDate}
+                  onChange={(e) => setPurchaseDate(e.target.value)}
+                />
+              </FormControl>
+              <Button type="submit" colorScheme="blue" w="full">
+                保存
+              </Button>
+            </VStack>
           </Box>
-        </Box>
 
-        {/* Updated navigation buttons */}
-        <HStack spacing={2} justifyContent="center" mt={6}>
-          <Link to="/" style={{ flex: 1 }}>
-            <Button colorScheme="teal" size={buttonSize} w="full">ホーム</Button>
-          </Link>
-          <Link to="/person" style={{ flex: 1 }}>
-            <Button colorScheme="blue" size={buttonSize} w="full">個人画面</Button>
-          </Link>
-          <Link to="/manage" style={{ flex: 1 }}>
-            <Button colorScheme="green" size={buttonSize} w="full">消耗品登録</Button>
-          </Link>
-          <Link to="/log" style={{ flex: 1 }}>
-            <Button colorScheme="red" size={buttonSize} w="full">購入品入力</Button>
-          </Link>
-        </HStack>
-      </VStack>
-    </Container>
+          <Box>
+            <Heading as="h2" size="lg" mb={4} fontSize="xl">
+              購入履歴
+            </Heading>
+            <Box overflowX="auto">
+              <Table variant="simple" size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>商品名</Th>
+                    <Th>数量</Th>
+                    <Th>購入日</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {purchases.map((purchase) => (
+                    <Tr key={purchase.id}>
+                      <Td>{purchase.productName}</Td>
+                      <Td>{purchase.quantity}</Td>
+                      <Td>{purchase.purchaseDate}</Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </Box>
+          </Box>
+        </VStack>
+      </Container>
+
+      {/* Navigation bar at the bottom */}
+      <Box 
+        position="fixed" 
+        bottom={0} 
+        left={0} 
+        right={0} 
+        bg="white" 
+        boxShadow="0 -2px 10px rgba(0, 0, 0, 0.1)"
+      >
+        <Container maxW="container.xl">
+          <HStack justifyContent="space-around" py={2}>
+            <Link to="/" style={{ flex: 1 }}>
+              <Button colorScheme="teal" size={buttonSize} w="full">ホーム</Button>
+            </Link>
+            <Link to="/person" style={{ flex: 1 }}>
+              <Button colorScheme="blue" size={buttonSize} w="full">個人画面</Button>
+            </Link>
+            <Link to="/manage" style={{ flex: 1 }}>
+              <Button colorScheme="green" size={buttonSize} w="full">消耗品登録</Button>
+            </Link>
+            <Link to="/log" style={{ flex: 1 }}>
+              <Button colorScheme="red" size={buttonSize} w="full">購入品入力</Button>
+            </Link>
+          </HStack>
+        </Container>
+      </Box>
+    </Box>
   );
 };
 
